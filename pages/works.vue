f0a2e953eb25057869238f6fc282d26babd04da3
unknown <rt1226.tennis@gmail.com>
2021-09-09 11:57:28 +0900

    fixed works.vue

--- pages/works.vue
+++ pages/works.vue
@@ -2,51 +2,55 @@
   <v-container class="px-0" fluid style="max-width: 1400px">
     <div class="ma-0">
       <v-row justify="center">
-        <v-col cols="12" md="4" lg="3" class="text-center">
-          <v-dialog v-model="dialog" width="800">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+        <v-dialog
+          v-model="dialog"
+          width="800"
+          v-for="(item, index) in Names"
+          :key="index"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <!-- <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
                 Click Me
+              </v-btn> -->
+            <v-col cols="12" md="4" lg="3" class="text-center">
+              <v-card v-bind="attrs" v-on="on">
+                <v-card-text>
+                  {{ item }}
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </template>
+
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              {{ item }}
+            </v-card-title>
+
+            <v-card-text>
+              aaaaaaaaaaaa
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="dialog = false">
+                I accept
               </v-btn>
-            </template>
-
-            <v-card>
-              <v-card-title class="text-h5 grey lighten-2">
-                Privacy Policy
-              </v-card-title>
-
-              <v-card-text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </v-card-text>
-
-              <v-divider></v-divider>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" text @click="dialog = false">
-                  I accept
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-col>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-row>
     </div>
   </v-container>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        dialog: false,
-      }
-    },
+export default {
+  data() {
+    return {
+      dialog: false,
+      Names: ["CCF(Create CSV File)", "Household-Account-Book", "Portfolio"],
+    };
   }
+};
 </script>
-

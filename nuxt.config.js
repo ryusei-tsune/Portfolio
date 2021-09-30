345b6a03bc69776f6b6da54b54c3e6eacc1d4a1b
unknown <rt1226.tennis@gmail.com>
2021-09-30 18:10:47 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,5 +82,9 @@ export default {
         ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
       ]
     }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
   }
 }
